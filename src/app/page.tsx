0d09492180ea963ebb11fe4bddf3b6b6f5ec23cd
nacica <mikio.yokohama@gmail.com>
2023-12-29 19:37:17 +0900

    verselデプロイ5回目

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,7 @@ export default async function Home() {
   const session = await getServerSession(nextAuthOptions);
   const user = session?.user as User;
 
-
-
   let purchaseBookIds: string[] = [];
-
 
 
   if(user){
